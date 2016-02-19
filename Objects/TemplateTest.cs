7ee31004efb1a8c89fe92464a4da0d2466420df8
Veronica <veronica.aubry.alley@gmail.com>
2016-02-19 08:08:49 -0800

    add template files

---
+++ Objects/TemplateTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+namespace  TEMPLATES.Objects
+{
+  public class TemplateTest
+  {
+    [Fact]
+    public void IsTemplateTest_ForCondition_true()
+    {
+      //code goes here//
+    }
+
+  }
+}
